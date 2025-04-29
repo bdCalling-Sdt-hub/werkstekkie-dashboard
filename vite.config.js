7ddac5fdb598f898c3c14fbd18f69a229b513001
Aminulhoque01 <aminuhoque53@gmail.com>
2025-04-29 18:09:57 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  optimizeDeps: {
+    include: ['react-quill'],
+  },
   plugins: [react()],
-  server:{
-    host:"0.0.0.0",
-    port:8080,
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
   }
 })
