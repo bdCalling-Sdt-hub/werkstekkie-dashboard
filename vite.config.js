067788d5186a56fe0fe0f306c28ca04949a00def
Aminulhoque01 <aminuhoque53@gmail.com>
2025-04-29 18:02:51 +0600

    css

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-quill/dist/quill.snow.css']
-    }
-  },
+
   plugins: [react()],
   server:{
     host:"0.0.0.0",
